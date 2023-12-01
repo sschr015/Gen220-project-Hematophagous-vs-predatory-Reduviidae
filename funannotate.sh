bb1334b4e02eac74c7b14c179899168aeea105a4
Veronica Tyts <vtyts001@jupyter2a.clust.hpcc.ucr.edu>
2023-11-30 21:46:51 -0800

    funannotate busco db insecta

--- funannotate.sh
+++ funannotate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash -l
-#SBATCH --mem 60G -c 32 --out funannotate_run_Zelurus.log
+#SBATCH --mem 65G -c 32 --out funannotate_run_Cavernicola.log
 module load funannotate
-funannotate sort -i ./raw_data/Zelurus_nigrolineatus_scaffolds.fasta -o ./raw_data/Zelurus_nigrolineatus_scaffolds_sort.fasta --minlen 5000
-funannotate mask -i ./raw_data/Zelurus_nigrolineatus_scaffolds_sort.fasta -o ./raw_data/Zelurus_nigrolineatus_scaffolds_masked.fasta --cpus 8
-funannotate predict -i ./raw_data/Zelurus_nigrolineatus_scaffolds_masked.fasta -o Zelurus_nigrolineatus_funannotate -s "Zelurus nigrolineatus" --transcript_evidence ./raw_data/TriPr.Trinity.fasta --cpus 32 --augustus_species drosophila --busco_db eukaryota_odb10 --weights snap:0 genemark:0
+funannotate sort -i ./raw_data/Cavernicola_scaffolds.fasta -o ./raw_data/Cavernicola_scaffolds_sort.fasta --minlen 5000
+funannotate mask -i ./raw_data/Cavernicola_scaffolds_sort.fasta -o ./raw_data/Cavernicola_scaffolds_masked.fasta --cpus 32
+funannotate predict -i ./raw_data/Cavernicola_scaffolds_masked.fasta -o Cavernicola_funannotate -s "Cavernicola sp" --transcript_evidence ./raw_data/TriPr.Trinity.fasta --cpus 32 --augustus_species drosophila --busco_db insecta_odb10 --weights snap:0 genemark:0
