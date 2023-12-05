ab9722215b8a12c42f46ba091ca228c8d3b64c19
Sarah Schroeder <sschr015@jupyter2a.clust.hpcc.ucr.edu>
2023-12-04 17:22:33 -0800

    orthofinder out

--- orthofinder_out.py
+++ orthofinder_out.py
@@ -14,19 +14,19 @@ folder = args.out
 df = pd.read_csv(args.file, sep="\t")
 #Homalocoris_erythrogaster is outgroup
 
-type_phy = df[(df['Homalocoris_erythrogaster'] == 1) & (df['Opisthacidius'] == 1) & (df['Zeluroides_americanus'] == 1) & (df['Zelurus_nigrolineatus'] == 1) & (df['Cavernicola'] == 1) & (df['Rhodnius_prolixus'] == 1)]
-type_gain = df[(df['Opisthacidius'] == 0) & (df['Zeluroides_americanus'] == 0) & (df['Zelurus_nigrolineatus'] == 0) & (df['Cavernicola'] > 0) & (df['Rhodnius_prolixus'] > 0)]
-type_gainM = type_gain[(type_gain['Cavernicola'] > 1) | (type_gain['Rhodnius_prolixus'] > 1)]
-type_gainS = type_gain[(type_gain['Cavernicola'] == 1) & (type_gain['Rhodnius_prolixus'] == 1)]
-logic_for_lost = ((df['Opisthacidius'] > 0) & ((df['Zeluroides_americanus'] > 0) | (df['Zelurus_nigrolineatus'] > 0)) | ((df['Zeluroides_americanus'] > 0) & (df['Zelurus_nigrolineatus'] > 0)))
-type_lost = df[logic_for_lost & (df['Cavernicola'] == 1) & (df['Rhodnius_prolixus'] == 1)]
-type_lostM = type_lost[(type_lost['Opisthacidius'] > 1) | (type_lost['Zeluroides_americanus'] > 1) | (type_lost['Zelurus_nigrolineatus'] > 1)]
-type_lostS = type_lost[(type_lost['Opisthacidius'] == 1) & (type_lost['Zeluroides_americanus'] == 1) | (type_lost['Zelurus_nigrolineatus'] == 1))]
+type_phy = df[(df['Homalocoris_erythrogaster.proteins'] == 1) & (df['Opisthacidius.proteins'] == 1) & (df['Zeluroides_americanus.proteins'] == 1) & (df['Zelurus_nigrolineatus.proteins'] == 1) & (df['Cavernicola.proteins'] == 1) & (df['rhodnius-prolixus-cdcpeptidesrproc33'] == 1)]
+type_gain = df[(df['Opisthacidius.proteins'] == 0) & (df['Zeluroides_americanus.proteins'] == 0) & (df['Zelurus_nigrolineatus.proteins'] == 0) & (df['Cavernicola.proteins'] > 0) & (df['rhodnius-prolixus-cdcpeptidesrproc33'] > 0)]
+type_gainM = type_gain[(type_gain['Cavernicola.proteins'] > 1) | (type_gain['rhodnius-prolixus-cdcpeptidesrproc33'] > 1)]
+type_gainS = type_gain[(type_gain['Cavernicola.proteins'] == 1) & (type_gain['rhodnius-prolixus-cdcpeptidesrproc33'] == 1)]
+logic_for_lost = ((df['Opisthacidius.proteins'] > 0) & ((df['Zeluroides_americanus.proteins'] > 0) | (df['Zelurus_nigrolineatus.proteins'] > 0)) | ((df['Zeluroides_americanus.proteins'] > 0) & (df['Zelurus_nigrolineatus.proteins'] > 0)))
+type_lost = df[logic_for_lost & (df['Cavernicola.proteins'] == 0) & (df['rhodnius-prolixus-cdcpeptidesrproc33'] == 0)]
+type_lostM = type_lost[(type_lost['Opisthacidius.proteins'] > 1) | (type_lost['Zeluroides_americanus.proteins'] > 1) | (type_lost['Zelurus_nigrolineatus.proteins'] > 1)]
+type_lostS = type_lost[(type_lost['Opisthacidius.proteins'] == 1) & (type_lost['Zeluroides_americanus.proteins'] == 1) & (type_lost['Zelurus_nigrolineatus.proteins'] == 1)]
 
-type_phy.to_csv("type_1.tsv", sep="\t", index=False)
-type_gain.to_csv("type_2.tsv", sep="\t", index=False)
-type_gainS.to_csv("type_2a.tsv", sep="\t", index=False)
-type_gainM.to_csv("type_2b.tsv", sep="\t", index=False)
-type_lost.to_csv("type_3.tsv", sep="\t", index=False)
-type_lostS.to_csv("type_3a.tsv", sep="\t", index=False)
-type_lostM.to_csv("type_3b.tsv", sep="\t", index=False)
+type_phy.to_csv(f"{folder}/type_phy.tsv", sep="\t", index=False)
+type_gain.to_csv(f"{folder}/type_gain.tsv", sep="\t", index=False)
+type_gainS.to_csv(f"{folder}/type_gainS.tsv", sep="\t", index=False)
+type_gainM.to_csv(f"{folder}/type_gainM.tsv", sep="\t", index=False)
+type_lost.to_csv(f"{folder}/type_lost.tsv", sep="\t", index=False)
+type_lostS.to_csv(f"{folder}/type_lostS.tsv", sep="\t", index=False)
+type_lostM.to_csv(f"{folder}/type_lostM.tsv", sep="\t", index=False)
