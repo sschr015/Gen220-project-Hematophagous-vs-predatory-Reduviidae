7b5331e184ad7cee7b6d497e65f9c28448045be5
Sarah Schroeder <sschr015@jupyter2a.clust.hpcc.ucr.edu>
2023-12-04 17:11:29 -0800

    align and trim

---
+++ align_and_trim.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash -l
+#SBATCH --ntasks 32 --mem 65G --out tree_%A.log
+module load muscle
+module load IQ-TREE/2.1.1
+module load trimal
+
+DIR=$1
+ALDIR=$2
+mkdir $ALDIR'_muscle'
+mkdir $ALDIR'_trimmed'
+for file in $(ls $DIR)
+    do
+    muscle -align $DIR'/'$file -output $ALDIR'_muscle/'$file'.efa'
+    trimal -automated1 -in $ALDIR'_muscle/'$file'.efa' -out $ALDIR'_trimmed/'$file'.phy' -phylip
+    done
+
+iqtree2 -S $ALDIR'_trimmed' -nt AUTO -bb 1000 -alrt 1000
