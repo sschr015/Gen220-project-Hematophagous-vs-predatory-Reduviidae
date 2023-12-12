66cab8e67bea82f34186f311923e4442f68bbf1c
Veronica Tyts <vtyts001@jupyter2a.clust.hpcc.ucr.edu>
2023-12-12 14:42:03 -0800

    removed treefile, minor changes in trinity.sh

--- trinity.sh
+++ trinity.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash -l
-#SBATCH --ntasks 32 --mem 20G -p short
+#SBATCH --ntasks 32 --mem 20G
 module load trinity-rnaseq
 module switch perl/5.22.0
 Trinity --seqType fq --left ./raw_data/TriPr_F1.fastq --right ./raw_data/TriPr_F2.fastq --CPU 8 --max_memory 20G
